bc8b8509895584a6bc8aee0b31d0693aadddcc4d
IamVDD <vodinhdat03@gmail.com>
2023-07-05 20:29:57 +0700

    add animation

--- sourcecode/src/controller/MainSimulationController.java
+++ sourcecode/src/controller/MainSimulationController.java
@@ -1,5 +1,20 @@
 package controller;
 
-public class MainSimulationController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.TranslateTransition;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.Initializable;
+import controller.AnimationController;
+public class MainSimulationController implements Initializable {
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        AnimationController animation = new AnimationController();
+        TranslateTransition surfaceTransition = new TranslateTransition();
+        TranslateTransition backgroundTransition = new TranslateTransition(); 
+        
+
+    }
     
 }
